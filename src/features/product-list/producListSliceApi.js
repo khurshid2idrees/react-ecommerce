9fe881eb8665ba662d3fc83dc6da6d92a7e508b9
Khurshid idrees <umarkhurshid3@gmail.com>
2023-07-24 18:01:21 +0530

    Part 2 : Added Routing (Login/Signup done)

--- src/features/product-list/producListSliceApi.js
+++ src/features/product-list/producListSliceApi.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './ProductListApi';
 
 const initialState = {
   value: 0,
@@ -39,7 +39,7 @@ export const counterSlice = createSlice({
         state.status = 'idle';
         state.value += action.payload;
       });
-  }},
+   } },
 });
 
 export const { increment} = counterSlice.actions;
